a96b8e2b51222079987c78101496fbd8afcec4b1
deadlydog <deadlydog@hotmail.com>
2025-02-12 17:44:59 -0600

    refactor: Move tests to a common file that can be used for dev tests and smoke tests

---
+++ deploy/GitClean.PublicNoMocksTests.ps1
@@ -0,0 +1,123 @@
+# These tests are in their own file so they can be easily dot-sourced into the local development tests
+# and the smoke tests without duplicating all of the code.
+# Because these are called by the smoke tests, make sure they only call public module functions and do not use mocks.
+# These are integration tests that assume git.exe is installed and available in the PATH environment variable.
+
+Describe 'Clean-GitRepositories' {
+	BeforeAll {
+		[string] $UntrackedFileName = 'UntrackedFile.txt'
+
+		function NewRandomRootDirectoryPath() {
+			[string] $rootDirectoryPath = "$TestDrive\" + ([System.IO.Path]::GetRandomFileName().Split('.')[0])
+			New-Item -Path $rootDirectoryPath -ItemType Directory -Force > $null
+			return $rootDirectoryPath
+		}
+
+		function NewRandomDirectoryPath([string] $rootDirectoryPath) {
+			[string] $directoryPath = Join-Path -Path $rootDirectoryPath -ChildPath ([System.IO.Path]::GetRandomFileName().Split('.')[0])
+			New-Item -Path $directoryPath -ItemType Directory -Force > $null
+			return $directoryPath
+		}
+
+		function CreateGitRepository([string] $directoryPath, [switch] $hasUntrackedFile) {
+			New-Item -Path $directoryPath -ItemType Directory -Force > $null
+			& git -C "$directoryPath" init > $null
+
+			if ($hasUntrackedFile) {
+				New-Item -Path (Join-Path -Path $directoryPath -ChildPath $UntrackedFileName) -ItemType File > $null
+			}
+		}
+	}
+
+	Context 'There is a single git repository' {
+		BeforeEach {
+			$RootDirectoryPath = NewRandomRootDirectoryPath
+			$Repo1Path = NewRandomDirectoryPath -rootDirectoryPath $RootDirectoryPath
+
+			[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '', Justification = 'Used in the It blocks')]
+			$Repo1UntrackedFilePath = Join-Path -Path $Repo1Path -ChildPath $UntrackedFileName
+		}
+
+		It 'Should clean the git repository if there are no untracked files' {
+			# Arrange.
+			CreateGitRepository -directoryPath $Repo1Path
+
+			# Act.
+			Clean-GitRepositories -RootDirectoryPath $RootDirectoryPath
+
+			# Assert.
+			$untrackedFileExists = Test-Path -Path $Repo1UntrackedFilePath
+			$untrackedFileExists | Should -Be $false
+		}
+
+		It 'Should not clean the git repository if there are untracked files' {
+			# Arrange.
+			CreateGitRepository -directoryPath $Repo1Path -hasUntrackedFile
+
+			# Act.
+			Clean-GitRepositories -RootDirectoryPath $RootDirectoryPath
+
+			# Assert.
+			$untrackedFileExists = Test-Path -Path $Repo1UntrackedFilePath
+			$untrackedFileExists | Should -Be $true
+		}
+
+		It 'Should clean the git repository if there are untracked files and the Force switch was used' {
+			# Arrange.
+			CreateGitRepository -directoryPath $Repo1Path -hasUntrackedFile
+
+			# Act.
+			Clean-GitRepositories -RootDirectoryPath $RootDirectoryPath -Force
+
+			# Assert.
+			$untrackedFileExists = Test-Path -Path $Repo1UntrackedFilePath
+			$untrackedFileExists | Should -Be $false
+		}
+
+		It 'Should not clean the git repository if the WhatIf switch is provided' {
+			# Arrange.
+			CreateGitRepository -directoryPath $Repo1Path -hasUntrackedFile
+
+			# Act.
+			Clean-GitRepositories -RootDirectoryPath $RootDirectoryPath -Force -WhatIf
+
+			# Assert.
+			$untrackedFileExists = Test-Path -Path $Repo1UntrackedFilePath
+			$untrackedFileExists | Should -Be $true
+		}
+	}
+
+	Context 'There are 3 git repositories' {
+		BeforeEach {
+			$RootDirectoryPath = NewRandomRootDirectoryPath
+			$Repo1Path = NewRandomDirectoryPath -rootDirectoryPath $RootDirectoryPath
+			$Repo2Path = NewRandomDirectoryPath -rootDirectoryPath $RootDirectoryPath
+			$Repo3Path = NewRandomDirectoryPath -rootDirectoryPath $RootDirectoryPath
+
+			[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '', Justification = 'Used in the It blocks')]
+			$Repo1UntrackedFilePath = Join-Path -Path $Repo1Path -ChildPath $UntrackedFileName
+			[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '', Justification = 'Used in the It blocks')]
+			$Repo2UntrackedFilePath = Join-Path -Path $Repo2Path -ChildPath $UntrackedFileName
+			[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '', Justification = 'Used in the It blocks')]
+			$Repo3UntrackedFilePath = Join-Path -Path $Repo3Path -ChildPath $UntrackedFileName
+		}
+
+		It 'Should only clean the git repositories that have no untracked files' {
+			# Arrange.
+			CreateGitRepository -directoryPath $Repo1Path
+			CreateGitRepository -directoryPath $Repo2Path -hasUntrackedFile
+			CreateGitRepository -directoryPath $Repo3Path
+
+			# Act.
+			Clean-GitRepositories -RootDirectoryPath $RootDirectoryPath
+
+			# Assert.
+			$untrackedFileExists1 = Test-Path -Path $Repo1UntrackedFilePath
+			$untrackedFileExists2 = Test-Path -Path $Repo2UntrackedFilePath
+			$untrackedFileExists3 = Test-Path -Path $Repo3UntrackedFilePath
+			$untrackedFileExists1 | Should -Be $false
+			$untrackedFileExists2 | Should -Be $true
+			$untrackedFileExists3 | Should -Be $false
+		}
+	}
+}
